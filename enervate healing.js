724dc5d8559c6ee824ae9c94bb4e3525adfdb9e8
Chris Allbritton <callbritton@mac.com>
2023-05-15 14:44:26 -0400

    initial commit

---
+++ enervate healing.js
@@ -0,0 +1,18 @@
+const lastArg = args[ args.length - 1 ];
+if ( lastArg.hitTargets.length === 0 ) return {};
+let actorD = game.actors.get( lastArg.actor._id );
+let tokenD = canvas.tokens.get( lastArg.tokenId );
+let target = canvas.tokens.get( lastArg.hitTargets[ 0 ].id );
+let healingType = "healing";
+let damageTotal = Math.floor( lastArg.damageTotal / 2 );
+let damageRoll = new Roll( `${damageTotal}` ).evaluate( { async: false } );
+new MidiQOL.DamageOnlyWorkflow( actorD, tokenD, damageRoll.total, healingType, [ tokenD ], damageRoll, { flavor: `(${CONFIG.DND5E.healingTypes[healingType]})`, itemCardId: lastArg.itemCardId, damageList: lastArg.damageList } );
+let targetList = `<div class="midi-qol-flex-container"><div class="midi-qol-target-npc midi-qol-target-name" id="${target.id}">hits ${target.name}</div><div><img src="${target.data.texture.src}" width="30" height="30" style="border:0px"></div></div><div class="midi-qol-flex-container"><div class="midi-qol-target-npc midi-qol-target-name" id="${tokenD.id}">heals ${tokenD.name}</div><div><img src="${tokenD.data.texture.src}" width="30" height="30" style="border:0px"></div></div>`;
+await warpgate.wait( 500 );
+const chatMessage = await game.messages.get( args[ 0 ].itemCardId );
+let content = await duplicate( chatMessage.data.content );
+const searchString = /<div class="midi-qol-hits-display">[\s\S]*<div class="end-midi-qol-hits-display">/g;
+const replaceString = `<div class="midi-qol-hits-display"><div class="end-midi-qol-hits-display">${targetList}`;
+content = await content.replace( searchString, replaceString );
+await chatMessage.update( { content: content } );
+await ui.chat.scrollBottom( );
---
+++ enervate item macro.js
@@ -0,0 +1,66 @@
+let item = args[ 1 ];
+console.log( "item:", item );
+
+
+let tokenDoc = canvas.scene.tokens.get( args[ args.length - 1 ].tokenId ); //Gets Token ID
+console.log( "tokenDoc:", tokenDoc );
+
+if ( args[ 0 ] === "on" ) {
+	//Updates to Token DELETE ANY UNNEEDED SECTIONS
+	const updates = {
+		embedded: {
+			Item: {
+				"Enervation - At Will Attack": { //String
+					type: "spell", // feat, spell
+					img: "icons/commodities/biological/eye-tentacle-grey-orange.webp", //Example: "icons/magic/holy/projectiles-blades-salvo-yellow.webp"
+					type: "spell",
+					flags: {
+						"midi-qol": {
+							onUseMacroName: "[postActiveEffects]Enervate Healing",
+						},
+					},
+					system: {
+						description: {
+							value: "<p>A tendril of inky darkness reaches out from you, touching a creature you can see within range to drain life from it. The target must make a Dexterity saving throw. On a successful save, the target takes 2d8 necrotic damage, and the spell ends. On a failed save, the target takes 4d8 necrotic damage, and until the spell ends, you can use your action on each of your turns to automatically deal 4d8 necrotic damage to the target. The spell ends if you use your action to do anything else, if the target is ever outside the spell’s range, or if the target has total cover from you.</p>\n<p>Whenever the spell deals damage to a target, you regain hit points equal to half the amount of necrotic damage the target takes.</p>\n<p><strong>At Higher Levels.</strong> When you cast this spell using a spell slot of 6th level or higher, the damage increases by 1d8 for each slot level above 5th.</p>", //String
+						},
+						activation: {
+							type: "action", // action, bonus, crew, day, hour, lair, legendary, minute, none, reaction, special, reactiondamage, reactionmanual
+							cost: 1, // Numeric
+							condition: "" // String
+						},
+						target: {
+							value: 1, // Numeric
+							width: null, // Numeric
+							units: "", // self, touch, spec, any, ft, mi, m, km
+							type: "creature" // ally, cone, cube, cylinder, enemy, line, none, object, radius, self, space, sphere, square, wall
+						},
+						range: {
+							value: 60, // Numeric
+							long: null, // Numeric
+							units: "ft" // self, touch, spec, any, ft, mi, m, km
+						},
+						actionType: "other", // mwak, rwak, msak, rsak, save, heal, abil, util, other
+						damage: {
+							parts: [
+								[ `${item - 1}`+"d8", "necrotic" ] // ["Formula", "acid, bludgeoning, cold, fire, force, lightning, necrotic, piercing, poison, psychic, radiant, slashing, thunder, healing, temphp"]
+							]
+						},
+						level: item, // Numeric 1-9
+						school: "nec", // abj, con, div, enc, evo, ill, nec, trs
+						preparation: {
+							mode: "atwill", // prepared, pact, always, atwill, innate
+							prepared: true // true, false
+						},
+						scaling: {
+							mode: "level", // cantrip, none, level
+							formula: "1d8" // Formula
+						}
+					}
+				}
+			}
+		}
+	}
+	await warpgate.mutate( tokenDoc, updates );
+} else if ( args[ 0 ] === "off" ) {
+	await warpgate.revert( tokenDoc );
+}
